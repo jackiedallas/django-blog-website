8405d77113772f939ef453adf49b68fe23c40760
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-02-21 14:35:22 -0500

    created landing.html

--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -1,24 +1,27 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect  # type: ignore
+from django.contrib.auth import login  # type: ignore
+from django.contrib.auth.forms import UserCreationForm  # type: ignore
 
 # Create your views here.
 
 # Landing Page
+
+
 def landing_page(request):
     if request.user.is_authenticated:
-        return redirect('home') # redirect logged in users to home page
+        return redirect('home')  # redirect logged in users to home page
     return render(request, 'blog/landing.html')
 
 # registration page
+
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user) # Log user in after registration
-            return redirect('home') # Redirect to home page
+            login(request, user)  # Log user in after registration
+            return redirect('home')  # Redirect to home page
     else:
         form = UserCreationForm()
     return render(request, 'blog/register.html', {'form': form})
-
