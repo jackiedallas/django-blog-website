1856305f84e6a60a81722661d555f2fb33374b21
Jackie Johnson-Dallas <j.johnson-dallas@afs.com>
2025-02-21 12:18:15 -0500

    defined landing page function

--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+# Landing Page
+def landing_page(request):
+    if request.user.is_authenticated:
+        return redirect('home') # redirect logged in users to home page
+    return render(request, 'blog/landing.html')
