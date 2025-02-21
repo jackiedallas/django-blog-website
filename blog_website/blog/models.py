606253a88cb2ce29044fba01c439e74bc7429f96
Jackie Johnson-Dallas <j.johnson-dallas@afs.com>
2025-02-21 11:48:12 -0500

    added blog post model

--- blog_website/blog/models.py
+++ blog_website/blog/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+from django.db import models # type: ignore
+from django.contrib.auth.models import User # type: ignore
+from django.utils.text import slugify # type: ignore
 
-# Create your models here.
+# Post Model
+class Post(models.Model):
+    """Class representing a post being made to the blog site."""
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=True)
+    
+    def save(self, *args, **kwargs):
+        """Method to save the post."""
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+        
+    def __str__(self):
+        """Method to return the post title."""
+        return self.title
+    
+
+
+
