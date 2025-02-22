be39ce142a09838f425070b01adb410bdbad0433
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-02-22 13:30:19 -0500

    added profile setting page functionality

--- blog_website/blog/urls.py
+++ blog_website/blog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.landing_page, name='landing'),
     path('home/', views.home, name='home'),
     path('register/', views.register, name='register'),
+    path('profile/settings/', views.profile_settings, name='profile_settings'),
     path('profile/<str:username>/', views.profile, name='profile'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),  # Add this for post details
-    path('profile/settings/', views.profile_settings, name='profile_settings'),
 ]
