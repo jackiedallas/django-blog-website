59778d7a634c4c4fb0dc43b00a9cdb97048f7767
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-02-22 13:24:52 -0500

    added url path for profile settings view

--- blog_website/blog/urls.py
+++ blog_website/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/<str:username>/', views.profile, name='profile'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),  # Add this for post details
+    path('profile/settings/', views.profile_settings, name='profile_settings'),
 ]
