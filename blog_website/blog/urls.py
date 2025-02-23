fb9ca5f7e9fdc0a097ea564e1f307562d1e09680
Jackie Dallas <jackie.r.dallas@gmail.com>
2025-02-23 11:57:38 -0500

    added delete route to url file

--- blog_website/blog/urls.py
+++ blog_website/blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/settings/', views.profile_settings, name='profile_settings'),
     path('profile/<str:username>/', views.profile, name='profile'),
+    path('post/delete/<int:post_id>/', views.delete_post, name='delete_post'),
     path('create-post/', views.create_post, name='create_post'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),  # Add this for post details
 ]
